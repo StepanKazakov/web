056e0221d8d455d69a3796108885279fe1dcd3b4
Stepan Kazakov <s.kazakov.1979@gmail.com>
2022-08-18 02:40:53 +0700

    finish second task

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
 def app(environ, start_response):
-        data = b"Hello, World!\n"
-        start_response("200 OK", [
-            ("Content-Type", "text/plain"),
-            ("Content-Length", str(len(data)))
-        ])
-        return iter([data])
-
+    start_response("200 OK", [("Content-Type", "text/plain")])
+    body=[bytes(i+'\n','ascii') for i in environ['QUERY_STRING'].split('&')]
+    return body
