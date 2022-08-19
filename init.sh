16b460db62dec805d4c0b595df3687289f740179
Stepan Kazakov <s.kazakov.1979@gmail.com>
2022-08-19 23:01:39 +0700

    modify

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
+#sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+#sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+#sudo /etc/init.d/gunicorn restart
 #sudo gunicorn -b 0.0.0.0:8080 hello:app &
 #sudo etc/init.d/mysql start
